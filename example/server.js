9677a06dfe9a3d33651c9c19c88a42af66be1ff8
Julio Velezmoro <julio28ve@gmail.com>
2025-04-16 12:29:09 +0100

    Format with `format` command.

--- example/server.js
+++ example/server.js
@@ -1,27 +1,37 @@
-const express = require('express');
-const { asyncWrapper, getErrorHandler, ejsErrorPages } = require('./../lib');
+const express = require("express");
+const { asyncWrapper, getErrorHandler, ejsErrorPages } = require("./../lib");
 
 const app = express();
 const urls = {
-  help: 'http://localhost:1234/help/contact',
-  assets: 'https://dev-cdn.signin.education.gov.uk',
-  assetsVersion: '1'
+  help: "http://localhost:1234/help/contact",
+  assets: "https://dev-cdn.signin.education.gov.uk",
+  assetsVersion: "1",
 };
 
-app.get('/greet', asyncWrapper((req, res) => {
-  const name = req.query.name || 'Unnamed requestor';
-  res.send(`Hello ${name}`);
-}));
+app.get(
+  "/greet",
+  asyncWrapper((req, res) => {
+    const name = req.query.name || "Unnamed requestor";
+    res.send(`Hello ${name}`);
+  }),
+);
 
-app.get('/broken', asyncWrapper(async (req, res) => {
-  return Promise.reject('This page has an async error');
-}));
+app.get(
+  "/broken",
+  asyncWrapper(async (req, res) => {
+    return Promise.reject("This page has an async error");
+  }),
+);
 
-app.use(getErrorHandler({
-  logger: console,
-  errorPageRenderer: ejsErrorPages.getErrorPageRenderer(urls, process.env.NODE_ENV === 'dev'),
-}));
-
+app.use(
+  getErrorHandler({
+    logger: console,
+    errorPageRenderer: ejsErrorPages.getErrorPageRenderer(
+      urls,
+      process.env.NODE_ENV === "dev",
+    ),
+  }),
+);
 
 let port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
 if (!port || isNaN(port)) {
@@ -30,4 +40,3 @@ if (!port || isNaN(port)) {
 app.listen(port, () => {
   console.info(`listening at http://localhost:${port}`);
 });
-
